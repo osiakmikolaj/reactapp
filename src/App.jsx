71e932db672129891fdd7b474c1523f3e2b42cba
Mikołaj Osiak <osiakmikolaj@yahoo.com>
2023-04-10 15:07:57 +0200

    My spinning name

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import logo from "./logo192.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="name">
+        <img className="logo" src={logo} alt="logo" height={50} /> Mikołaj{" "}
+        <img className="logo" src={logo} alt="logo" height={50} />
+      </div>
+    </div>
+  );
+}
